3cec8ea20f5ba443af95ae3f184eba51dd8b3fd5
kpokharel59 <kpokharel59@gmail.com>
2016-11-27 19:17:07 -0500

    Adding run script

---
+++ run.sh
@@ -0,0 +1,8 @@
+echo "Running time for Original Code"
+./norm
+echo "Running time for Optimization 1"
+./norm_opt1
+echo "Running time for Optimization 2"
+./norm_opt2
+echo "Running time for Optimization 3"
+./norm_opt3
